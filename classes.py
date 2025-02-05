88d7be54712ef7a9107ba62fe7654797c760cea5
Ian Sternkopf <iansternkopf@protonmail.com>
2025-02-04 22:06:14 -0500

    yep

--- classes.py
+++ classes.py
@@ -1,1 +1,58 @@
+import pygame
 
+class Entity:
+    def __init__(self, position, collisionDetection, hitboxes, displayImages):
+        self.position = position
+        self.detectCollision = collisionDetection
+        self.hitboxes = hitboxes
+        self.displayImages = displayImages
+        self.currentImage = 0
+
+class Player(Entity):
+    def __init__(self, position, hp, maxhp, detectCollision, hitboxes, displayImages,speed):
+        super().__init__(position, detectCollision, hitboxes, displayImages)
+        self.hp = hp
+        self.maxhp = maxhp
+        self.speed = speed
+    def update(self,fps):
+        diff = pygame.mouse.get_pos()
+        translation = [diff[i]-self.position[i]%(self.speed/fps) for i in [0,1]]
+        self.position = [self.position[i]+translation[i] for i in [0,1]]
+    def checkCollisions(self,objects):
+        for i in self.hitboxes:
+            if i.Rect.collidelist(objects):
+                return True
+        return False
+
+class Projectile(Entity):
+    def __init__(self, startingMomentum, position, hitboxes, collisionDetection, displayImages,friendly,impactDeath,speed):
+        super().__init__(position, collisionDetection, hitboxes, displayImages)
+        self.momentum = startingMomentum
+        self.friendly = friendly
+        self.dieOnImpact = impactDeath
+        self.speed = speed
+
+class EntityHandler:
+    def __init__(self):
+        self.elements = ['empty']
+        self.availableIndexes = [0]
+    def scan(self):
+        for i in range(len(self.elements)):
+            if self.elements[i] == 'empty':
+                self.availableIndexes.append(i)
+            elif i in self.availableIndexes:
+                self.availableIndexes.remove(i)
+            if len(self.availableIndexes) == 0:
+                self.elements.append('empty')
+                self.availableIndexes.append(len(self.elements)-1)
+        return self.availableIndexes
+    def add(self,element,index):
+        self.elements[index] = element
+        self.availableIndexes.remove(index)
+    def remove(self,index):
+        self.elements[index] = 'empty'
+        self.availableIndexes.append(index)
+    def update(self):
+        for element in self.elements:
+            if isinstance(element,Entity):
+                element.update()
